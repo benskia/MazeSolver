a44513a70ed01006e609be60e4c7258dfb506d13
benskia <benski.abraham@gmail.com>
2024-05-21 18:53:35 -0500

    add test lines to main

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from graphics import Window
+from graphics import Window, Point, Line
+
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(0, 0), Point(100, 100))
+    line2 = Line(Point(50, 50), Point(-200, -200))
+    win.create_line(line1, "red")
+    win.create_line(line2, "blue")
     win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
