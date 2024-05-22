b4f00ac93fc72ea88b80a53b3d5fabc7c1272282
benskia <benski.abraham@gmail.com>
2024-05-21 19:32:44 -0500

    update graphics

--- graphics.py
+++ graphics.py
@@ -5,9 +5,9 @@ class Window:
     def __init__(self, width, height):
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root, bg="white", width=width, height=height)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.is_running = False
+        self.__canvas = Canvas(self.__root, bg="white", width=width, height=height)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__is_running = False
         self.__root.wm_protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
@@ -15,15 +15,15 @@ class Window:
         self.__root.update()
 
     def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
+        self.__is_running = True
+        while self.__is_running:
             self.redraw()
 
     def close(self):
-        self.is_running = False
+        self.__is_running = False
 
     def create_line(self, line, fill_color):
-        line.draw(self.canvas, fill_color)
+        line.draw(self.__canvas, fill_color)
 
 
 class Point:
@@ -33,11 +33,28 @@ class Point:
 
 
 class Line:
-    def __init__(self, point1, point2):
-        self.pt1 = point1
-        self.pt2 = point2
+    def __init__(self, pt1, pt2):
+        self.pt1 = pt1
+        self.pt2 = pt2
 
     def draw(self, canvas, fill_color):
         canvas.create_line(
             self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y, fill=fill_color, width=2
         )
+
+
+class Cell:
+    def __init__(self, pt1, pt2, canvas):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = pt1.x
+        self._y1 = pt1.y
+        self._x2 = pt2.x
+        self._y2 = pt2.y
+        self._win = canvas
+
+    def draw(self, x1, y1, x2, y2):
+        if self.has_left_wall:
+            pass
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from graphics import Window, Point, Line
 def main():
     win = Window(800, 600)
     line1 = Line(Point(0, 0), Point(100, 100))
-    line2 = Line(Point(50, 50), Point(-200, -200))
+    line2 = Line(Point(100,100), Point(200, 200))
     win.create_line(line1, "red")
     win.create_line(line2, "blue")
     win.wait_for_close()
