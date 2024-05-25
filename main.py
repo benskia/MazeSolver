28ad52452c5075d482014915419f7eda6940b4f2
benskia <benski.abraham@gmail.com>
2024-05-25 17:34:50 -0500

    update maze

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from graphics import Window
 from cell import Cell
+from maze import Maze
 
 
 def main():
     win = Window(800, 600)
     
-    c = Cell(win)
+    maze = Maze(10, 10, 4, 4, 100, 100, win)
 
     win.wait_for_close()
 
--- maze.py
+++ maze.py
@@ -1,3 +1,4 @@
+from time import sleep
 from cell import Cell
 
 
@@ -11,16 +12,17 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        self._cells = self._create_cells()
 
     def _create_cells(self):
-        cells = []
+        self._cells = []
         for i in range(self._num_rows):
             row_cells = []
             for j in range(self._num_cols):
                 row_cells.append(Cell(self._win))
-            cells.append(row_cells)
-        return cells
+            self._cells.append(row_cells)
+        for i in range(len(self._cells)):
+            for j in range(len(self.cells[i])):
+                self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
         x1 = (j) * self._cell_size_x + self._x1
@@ -28,3 +30,8 @@ class Maze:
         x2 = (j + 1) * self._cell_size_x + self._x1
         y2 = (i + 1) * self._cell_size_y + self._y1
         self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        sleep(0.05)
