8226056e744d160e51af0b6a3a2e22717a02b694
benskia <benski.abraham@gmail.com>
2024-05-25 16:45:29 -0500

    move Cell coords from draw to constructor

--- cell.py
+++ cell.py
@@ -2,26 +2,22 @@ from graphics import Line, Point
 
 
 class Cell:
-    def __init__(self, canvas):
+    def __init__(self, canvas, x1, y1, x2, y2):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = None
-        self._y1 = None
-        self._x2 = None
-        self._y2 = None
-        self._win = canvas
-
-    def draw(self, x1, y1, x2, y2):
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
-        top_left = Point(x1, y1)
-        bot_left = Point(x1, y2)
-        top_right = Point(x2, y1)
-        bot_right = Point(x2, y2)
+        self._win = canvas
+
+    def draw(self):
+        top_left = Point(self._x1, self._y1)
+        bot_left = Point(self._x1, self._y2)
+        bot_right = Point(self._x2, self._y1)
+        top_right = Point(self._x2, self._y2)
         if self.has_left_wall:
             left_wall = Line(top_left, bot_left)
             self._win.create_line(left_wall)
@@ -34,3 +30,16 @@ class Cell:
         if self.has_bottom_wall:
             bottom_wall = Line(bot_left, bot_right)
             self._win.create_line(bottom_wall)
+
+    def draw_move(self, to_cell, undo=False):
+        if [v is None for v in [self._x1, self._y1, self._x2, self._y2]]:
+            return
+        current_center_x = self._x1 + (self._x2 - self._x1) // 2
+        current_center_y = self._y1 + (self._y2 - self._y1) // 2
+        current_center = Point(current_center_x, current_center_y)
+        next_center_x = to_cell._x1 + (to_cell._x2 - to_cell._x1) // 2
+        next_center_y = to_cell._y1 + (to_cell._y2 - to_cell._y1) // 2
+        next_center = Point(next_center_x, next_center_y)
+        line = Line(current_center, next_center)
+        fill_color = "gray" if undo else "red"
+        line.draw(self._win, fill_color)
--- main.py
+++ main.py
@@ -5,23 +5,25 @@ from cell import Cell
 def main():
     win = Window(800, 600)
     
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(100, 100, 200, 200)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(100, 100, 200, 200)
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.has_bottom_wall = False
-    c.draw(200, 100, 300, 200)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(200, 100, 300, 200)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.has_right_wall = False
-    c.draw(200, 200, 300, 300)
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(200, 200, 300, 300)
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(300, 200, 400, 300)
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(300, 200, 400, 300)
+
+    
 
     win.wait_for_close()
 
